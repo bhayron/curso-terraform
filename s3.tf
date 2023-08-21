9f89ad853b2b787c398301a401abe13bff940818
Bhayron <bhayronklivilan@gmail.com>
2023-08-21 20:13:51 -0300

    up

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket#private-bucket-w-tags
 resource "aws_s3_bucket" "my-test-bucket" {
-  bucket = "my-tf-test-bucket-bhayron"
+  bucket = "${random_pet.bucket.id}-bhayron"
 
   tags = {
     Name        = "My first Terraform bucket"
