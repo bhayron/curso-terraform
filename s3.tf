9d47893da35f42df06d5a796c254a6fef77dca37
Bhayron <bhayronklivilan@gmail.com>
2023-08-21 21:17:30 -0300

    up

--- s3.tf
+++ s3.tf
@@ -1,12 +1,10 @@
-
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket#private-bucket-w-tags
 resource "aws_s3_bucket" "my-test-bucket" {
   bucket = "${random_pet.bucket.id}-bhayron"
   tags   = local.common_tags
 }
 
-resource "aws_s3_bucket_object" "this" {
-  bucket = aws_s3_bucket.my-test-bucket.bucket
+resource "aws_s3_object" "this" {
+  bucket = aws_s3_bucket.my-test-bucket.id
   key    = "config/${local.ip_filepath}"
   source = local.ip_filepath
   etag   = filemd5(local.ip_filepath)
