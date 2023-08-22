fbc95cb91bf9c0c0d3582f0dda3f68b70729bb79
Bhayron <bhayronklivilan@gmail.com>
2023-08-21 21:49:36 -0300

    ajustes

---
+++ outputs.tf
@@ -0,0 +1,17 @@
+output "bucket_name" {
+  value = aws_s3_bucket.my-test-bucket.bucket
+}
+
+output "bucket_arn" {
+  value       = aws_s3_bucket.my-test-bucket.arn
+  description = ""
+}
+
+output "bucket_domain_name" {
+  value = aws_s3_bucket.my-test-bucket.bucket_domain_name
+}
+
+output "ips_file_path" {
+  value = "${aws_s3_bucket.my-test-bucket.bucket}/${local.ip_filepath}"
+}
+
